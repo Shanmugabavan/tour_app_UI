fb5300c130a08c92763d20848c8136b0dd630bea
Shanmugabavan <shanmugabavan25621@gmail.com>
2021-01-17 22:45:39 +0530

    UI design for uploading photo (Not implemented)

--- screens/editProfile.js
+++ screens/editProfile.js
@@ -18,157 +18,165 @@ import BottomSheet from 'reanimated-bottom-sheet';
 import Animated from 'react-native-reanimated';
 // import { styles } from './visitingAreas';
 
-export default class EditProfileScreen extends Component{
+const EditProfileScreen =()=>{
     bs=React.createRef();
     fall=new Animated.Value(1);
-    render(){
         
-        const renderInner=()=>(
-            <View style={styles.panel}>
+    renderInner=()=>(
+        <View style={styles.panel}>
+            <View style={{alignItems:'center'}}>
+                <Text style={styles.PanelTitle}>
+                    Upload Photo
+                </Text>
+                <Text style={styles.PanelSubtitle}>
+                    Choose your profile Picture
+                </Text>
+                <TouchableOpacity style={styles.panelButton}>
+                    <Text style={styles.panelButtonTitle}>
+                        Take Photo
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.panelButton}>
+                    <Text style={styles.panelButtonTitle}>
+                        Choose from Library
+                    </Text>
+                </TouchableOpacity>
+                {/* <TouchableOpacity style={styles.panelButton} onPress={()=>this.bs.current.snapTo(1)}>
+                    <Text style={styles.panelButtonTitle}>
+                        Cancel
+                    </Text>
+                </TouchableOpacity> */}
+            </View>
+        </View>
+        
+    );
+    renderHeader=()=>(
+        <View style={styles.header}>
+            <View style={styles.panelHeader}>
+                <View style={styles.panelHandle}>
+
+                </View>
+
+            </View>
+        </View>
+        
+    );
+    return(
+        <View style={styles.container}>
+            {/* <Header
+            // leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'Edit Profile', style: { color: '#fff',fontSize:30, fontWeight:"bold" } }}
+            // rightComponent={{ icon: 'home', color: '#fff' }}
+            /> */}
+            <BottomSheet
+                ref={this.bs}
+                snapPoints={[320,100,0]}
+                renderContent={this.renderInner}
+                renderHeader={this.renderHeader}
+                initialSnap={2}
+                callbackNode={this.fall}
+                enabledGestureInteraction={true}
+            />
+            <View style={{margin:20}}>
                 <View style={{alignItems:'center'}}>
-                    <Text style={styles.PanelTitle}>
-                        Upload Photo
-                    </Text>
-                    <Text style={styles.PanelSubtitle}>
-                        Choose your profile Picture
-                    </Text>
-                    <TouchableOpacity style={styles.panelButton}>
-                        <Text style={styles.panelButtonTitle}>
-                            Take Photo
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            
-        );
-        const renderHeader=()=>(
-            <View style={styles.header}>
-                <View style={styles.panelHeader}>
-                    <View style={styles.panelHandle}>
-    
-                    </View>
-    
-                </View>
-            </View>
-            
-        );
-        return(
-            <View style={styles.container}>
-                {/* <Header
-                // leftComponent={{ icon: 'menu', color: '#fff' }}
-                centerComponent={{ text: 'Edit Profile', style: { color: '#fff',fontSize:30, fontWeight:"bold" } }}
-                // rightComponent={{ icon: 'home', color: '#fff' }}
-                /> */}
-                <BottomSheet
-                    ref={this.bs}
-                    snapPoints={[300,100,0]}
-                    renderContent={this.renderInner}
-                    renderHeader={this.renderHeader}
-                    initialSnap={1}
-                    callbackNode={this.fall}
-                    enabledGestureInteraction={true}
-                />
-                <View style={{margin:20}}>
-                    <View style={{alignItems:'center'}}>
-                        <TouchableOpacity onPress={()=>this.bs.current.snapTo(0)}>
-                            <View style={{
-                                height:100,
-                                width:100,
-                                borderRadius:15,
-                                justifyContent:'center',
-                                alignItems:'center',
-                                backgroundColor:"black"
-                            }}>
-                                <ImageBackground
-                                    source={require("../assets/images/dampulla.jpg")}
-                                    style={{height:100,width:100}}
-                                    imageStyle={{borderRadius:15}}>
-                                        <View style={{
-                                            flex:1,
-                                            justifyContent:'center',
+                    <TouchableOpacity onPress={()=>this.bs.current.snapTo(0)}>
+                        <View style={{
+                            height:100,
+                            width:100,
+                            borderRadius:15,
+                            justifyContent:'center',
+                            alignItems:'center',
+                            backgroundColor:"black"
+                        }}>
+                            <ImageBackground
+                                source={require("../assets/images/dampulla.jpg")}
+                                style={{height:100,width:100}}
+                                imageStyle={{borderRadius:15}}>
+                                    <View style={{
+                                        flex:1,
+                                        justifyContent:'center',
+                                        alignItems:'center',
+                                    }}>
+                                        <Icon name='camera' size={35} color="#fff" style={{
+                                            opacity:0.7,
                                             alignItems:'center',
+                                            justifyContent:'center',
+                                            borderWidth:1,
+                                            borderColor:'#fff',
+                                            borderRadius:10,
                                         }}>
-                                            <Icon name='camera' size={35} color="#fff" style={{
-                                                opacity:0.7,
-                                                alignItems:'center',
-                                                justifyContent:'center',
-                                                borderWidth:1,
-                                                borderColor:'#fff',
-                                                borderRadius:10,
-                                            }}>
-    
-                                            </Icon>
-                                        </View>
-                        
-                                </ImageBackground>
-                            </View>
-                        </TouchableOpacity>
-                        <Text style={{marginTop:10,fontSize:18,fontWeight:'bold'}}>
-                            Shanmugabavan
-                        </Text>
-                    </View>
-    
-                    <View style={styles.action}>
-                        <FontAwesome name='user-o' size={20}/>
-                        <TextInput
-                            placeholder="Name"
-                            placeholderTextColor='#666666'
-                            style={styles.textInput}
-                        />
-                    </View>
-                    <View style={styles.action}>
-                        <Feather name='phone' size={20}/>
-                        <TextInput
-                            placeholder="Phone number"
-                            placeholderTextColor='#666666'
-                            style={styles.textInput}
-                        />
-                    </View>
-                    <View style={styles.action}>
-                        <FontAwesome name='user-o' size={20}/>
-                        <TextInput
-                            placeholder="NIC"
-                            placeholderTextColor='#666666'
-                            style={styles.textInput}
-                        />
-                    </View>
-                    <TouchableOpacity style={styles.CommandButton} onPress={()=>{}}>
-                        <Text style={styles.panelButtonTitle}>Submit</Text>
+
+                                        </Icon>
+                                    </View>
+                    
+                            </ImageBackground>
+                        </View>
                     </TouchableOpacity>
-    
-                    <View style={styles.action}>
-                        <FontAwesome name='user-o' size={20}/>
-                        <TextInput
-                            secureTextEntry={true}
-                            placeholder="Old Password"
-                            placeholderTextColor='#666666'
-                            style={styles.textInput}
-                        />
-                    </View>
-                    <View style={styles.action}>
-                        <FontAwesome name='user-o' size={20}/>
-                        <TextInput 
-                            secureTextEntry={true}
-                            placeholder="New Password"
-                            placeholderTextColor='#666666'
-                            style={styles.textInput}
-                        />
-                    </View>
-                    <TouchableOpacity style={styles.CommandButton} onPress={()=>{}}>
-                        <Text style={styles.panelButtonTitle}>Change Password</Text>
-                    </TouchableOpacity>
-    
-    
+                    <Text style={{marginTop:10,fontSize:18,fontWeight:'bold'}}>
+                        Shanmugabavan
+                    </Text>
                 </View>
-    
-    
+
+                <View style={styles.action}>
+                    <FontAwesome name='user-o' size={20}/>
+                    <TextInput
+                        placeholder="Name"
+                        placeholderTextColor='#666666'
+                        style={styles.textInput}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <Feather name='phone' size={20}/>
+                    <TextInput
+                        placeholder="Phone number"
+                        placeholderTextColor='#666666'
+                        style={styles.textInput}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name='user-o' size={20}/>
+                    <TextInput
+                        placeholder="NIC"
+                        placeholderTextColor='#666666'
+                        style={styles.textInput}
+                    />
+                </View>
+                <TouchableOpacity style={styles.CommandButton} onPress={()=>{}}>
+                    <Text style={styles.panelButtonTitle}>Submit</Text>
+                </TouchableOpacity>
+
+                <View style={styles.action}>
+                    <FontAwesome name='user-o' size={20}/>
+                    <TextInput
+                        secureTextEntry={true}
+                        placeholder="Old Password"
+                        placeholderTextColor='#666666'
+                        style={styles.textInput}
+                    />
+                </View>
+                <View style={styles.action}>
+                    <FontAwesome name='user-o' size={20}/>
+                    <TextInput 
+                        secureTextEntry={true}
+                        placeholder="New Password"
+                        placeholderTextColor='#666666'
+                        style={styles.textInput}
+                    />
+                </View>
+                <TouchableOpacity style={styles.CommandButton} onPress={()=>{}}>
+                    <Text style={styles.panelButtonTitle}>Change Password</Text>
+                </TouchableOpacity>
+
+
             </View>
-        );
+
+
+        </View>
+    );
 
     }
+export default EditProfileScreen;
     
-}
-
 
 
 const styles=StyleSheet.create({
@@ -220,6 +228,7 @@ const styles=StyleSheet.create({
     panelButton:{
         padding:13,
         borderRadius:10,
+        width:'100%',
         backgroundColor:"#FF6347",
         alignItems:'center',
         marginVertical:7,
